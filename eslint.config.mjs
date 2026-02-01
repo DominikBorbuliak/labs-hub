952e76929f01b2f729309abebdcf5d6ced1941ab
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2026-02-01 23:46:49 +0100

    feat: Added prettier and linter settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,47 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettier,
+  {
+    rules: {
+      "react/no-unescaped-entities": "warn",
+      "react/prop-types": "off",
+      "@next/next/no-img-element": "warn",
+      "@next/next/no-html-link-for-pages": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-types": "off",
+      "no-console": [
+        "warn",
+        {
+          allow: ["warn", "error"],
+        },
+      ],
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
+    "dist/**",
+    ".git/**",
+    "coverage/**",
   ]),
 ]);
 
