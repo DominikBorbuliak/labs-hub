435ee991012b1bc49043bd1fbe6e062a8f8a6217
Dominik Borbuliak <borbuliak.dominik@gmail.com>
2026-04-15 22:45:25 +0200

    feat: Added copy functionality to code blocks

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/table";
 import { Separator } from "@/components/ui/separator";
 import { Spoiler } from "@/components/spoiler";
+import { CodeBlock } from "@/components/code-block";
 
 const components: MDXComponents = {
   h1: ({ node: _node, ...props }) => (
@@ -77,12 +78,7 @@ const components: MDXComponents = {
       </code>
     );
   },
-  pre: ({ node: _node, ...props }) => (
-    <pre
-      className="bg-[#0d1117]! text-gray-100! p-3 rounded-lg overflow-x-auto mb-4 border-0"
-      {...props}
-    />
-  ),
+  pre: ({ node: _node, ...props }) => <CodeBlock {...props} />,
   blockquote: ({ node: _node, ...props }) => (
     <blockquote
       className="border-l-4 border-primary bg-muted pl-3 pr-3 py-1.5 italic my-4 text-muted-foreground"
